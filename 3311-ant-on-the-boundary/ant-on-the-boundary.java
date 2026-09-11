aa6fc045eb1d0588a30c2561a6239190cafa38d0
AKE SRIRAM <24p31a1201@acet.ac.in>
2026-09-11 18:42:24 +0530

    Time: 1 ms (18.80%) | Memory: 44.1 MB (13.57%) - LeetSync

---
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+       int sum=0,cnt=0;
+       for(int i=0;i<nums.length;i++) {
+          sum+=nums[i];
+          if(sum==0) cnt++;
+       }
+       return cnt;
+    }
+}
